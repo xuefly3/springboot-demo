377fe026c933b55635b2efe2475400e0f5464385
xuefly3 <xuefly9527@163.com>
2020-01-15 16:17:48 +0800

    集成 springCloud Eureka

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -14,6 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableScheduling
 @EnableTransactionManagement
 @Slf4j
+@EnableDiscoveryClient
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -21,4 +26,10 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    //启用负载均衡，默认算法是轮询
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
---
+++ src/main/java/com/example/demo/controller/ProducerController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 类功能描述：<br>
+ * <ul>
+ * <li>类功能描述1<br>
+ * <li>类功能描述2<br>
+ * <li>类功能描述3<br>
+ * </ul>
+ * 修改记录：<br>
+ * <ul>
+ * <li>修改记录描述1<br>
+ * <li>修改记录描述2<br>
+ * <li>修改记录描述3<br>
+ * </ul>
+ *
+ * @author xuefl
+ * @version 5.0 since 2020-01-15
+ */
+@RequestMapping("/producer")
+@RestController
+public class ProducerController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/get_user")
+    public String getAllUser(){
+        return userService.selectAll().toString();
+    }
+}
