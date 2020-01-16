0d9f8c966f81cca30133ca36b5196377f019cfd2
xuefly3 <xuefly9527@163.com>
2020-01-16 10:43:42 +0800

    目录整改

--- eureka/src/main/java/com/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/example/eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.example.cloud;
+package com.example.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class CloudApplication {
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 
 }
